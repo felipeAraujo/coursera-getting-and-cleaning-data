090af9b5a540cbe2af01c34f74a645d98e410f67
Felipe Teixeira <felipe.at.1989@gmail.com>
2018-12-06 00:30:21 -0200

    Grouping by activity and person

--- run_analysis.R
+++ run_analysis.R
@@ -2,8 +2,8 @@ source('exercise1.R')
 desired_answer <- function() {
     middle_data <- answer_1()
     middle_data <- middle_data %>%
-        select(activity:`fBodyBodyGyroJerkMag-meanFreq()`) %>%
-        group_by(activity) %>%
+        select(person:`fBodyBodyGyroJerkMag-meanFreq()`) %>%
+        group_by(activity, person) %>%
         summarise_all(funs(mean))
 
     middle_data
